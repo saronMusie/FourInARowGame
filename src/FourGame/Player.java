3c4791def0f24e888172362f456e50c85ae9b8ab
Saron <saronlebassi@gmail.com>
2023-01-30 21:17:12 -0700

    Player class added

---
+++ src/FourGame/Player.java
@@ -0,0 +1,28 @@
+package FourGame;
+
+import java.util.Scanner;
+class Player{
+    private String playerName;
+    private String playerNumber;
+    private static Scanner scanner = new Scanner(System.in);
+    public Player(String playerName, String playerNumber){
+        this.playerName = playerName;
+        this.playerNumber = playerNumber;
+    }
+    public String getName(){
+        return playerName;
+    }
+    public String getPlayerNumber(){
+        return playerNumber;
+    }
+    public int makeMove(){
+        System.out.println("Choose column");
+        int columnNum = scanner.nextInt();
+        scanner.nextLine();
+        scanner.close();
+        return columnNum;
+    }
+    public String toString(){
+        return ("Player "+playerNumber+" is "+playerName);
+    }
+}
